0c958a34bdac49e000b3cdd2dea77bfa95a749b1
Riona-Pragati <renuprasanna2012@gmail.com>
2026-01-07 20:17:48 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-for i in range(1, 11):
-   print("23 x", i,"=",23*i)
+def add(x,y):
+   return x + y
+def subtract(x,y):
+   return x - y
+def multiply(x,y):
+   return x * y
+def divide(x,y):
+   return x / y
+
+num1 = int(input("Enter a number1"))
+num2 = int(input("Enter a number2"))
+
+print("Sum: ", add(num1,num2))
+print("Difference: ", subtract(num1,num2))
+print("Product: ", multiply(num1,num2))
+print("Quotient: ", divide(num1,num2))
